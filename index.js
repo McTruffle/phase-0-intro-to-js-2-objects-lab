b078f54a8da22b77ae89e539e7702a63609e4699
McTruffle <56895893+McTruffle@users.noreply.github.com>
2022-05-26 10:36:58 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution in this file!
+// Write your solution in this file!
+const employee = {
+    name: [],
+    streetAddress: [],
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {
+        ...obj,
+        [key]: value,
+    };
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+
+    return obj;
+};
+
+function deleteFromEmployeeByKey(obj, key, value) {
+    return {
+        ...obj,
+        [key]: value,
+    };
+};
+
+function destructivelyDeleteFromEmployeeByKey(obj, key, value) {
+    obj[key] = value;
+    return obj;
+};
+
+/* 
+
+As you work on your functions, be sure to think about when 
+to use dot notation vs. bracket notation.
+*/
